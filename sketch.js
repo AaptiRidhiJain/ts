43e88a38252a15f8bff6ff061f82ae5c68bcb630
AaptiRidhiJain <72203592+AaptiRidhiJain@users.noreply.github.com>
2020-12-05 19:30:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+var a;
+var circles=[];
+var block8,block9,block10,block11,block12,block13,block14,block15,block16;
+
+function setup() {
+  createCanvas(800,800);
+  stroke(255)
+  
+ // camera=new Camera(width/2,height/2,0.5);
+  //camera.on();
+  a=height;
+  circles.push(width/2)
+
+  //level two
+  block8 = new Block(330,235,30,40);
+  block9 = new Block(360,235,30,40);
+
+  console.log(block8);
+  console.log(block9);x
+}
+
+function draw() {
+  //camera.zoom=camera.zoom+1
+  background(0);  
+  
+  
+  a=a-1;
+  //camera.zoom=camera.zoom+0.01
+  //camera.position={x:width/2,y:a}
+ 
+  
+  for (i=0;i<circles.length;i++)
+{
+	circle(circles[i], height/2,20)
+}
+if(camera.position.x%width===0)
+{
+	circles.push(camera.position.x+width/2)
+}
+
+    block8.display();
+    block9.display();    
+
+ // camera(0, 0, 20 + sin(frameCount * 0.01) * 10, 0, 0, 0, 0, 1, 0);
+ drawSprites();
+}
+
+function keyPressed ()
+{
+  if(keyCode === RIGHT_ARROW)
+  {
+    if(keyIsDown(RIGHT_ARROW))
+    {
+      camera.position.x=camera.position.x+10;
+    }
+  }
+} 
